451f25a69c54628beef8677927ff0d9d7dc4963a
vaibhav dhiman <princedhiman70@gmail.com>
2022-10-16 12:10:28 +0530

    input-tag

--- hotelinventory/src/app/rooms/rooms.component.ts
+++ hotelinventory/src/app/rooms/rooms.component.ts
@@ -26,6 +26,7 @@ export class RoomsComponent implements OnInit {
     photos: 'https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
     checkinTime: new Date('8-Dec-2022'),
     checkoutTIme: new Date('9-Dec-2022'),
+    rating: 3.4,
   },
   {
     roomNumber:  321,
@@ -35,6 +36,8 @@ export class RoomsComponent implements OnInit {
     photos: 'https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=958&q=80',
     checkinTime: new Date('3-Dec-2022'),
     checkoutTIme: new Date('9-Dec-2022'),
+    rating: 3.1,
+
   },
   {
     roomNumber:  32,
@@ -44,10 +47,14 @@ export class RoomsComponent implements OnInit {
     photos: 'https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
     checkinTime: new Date('17-Dec-2022'),
     checkoutTIme: new Date('9-Dec-2022'),
+    rating: 4.2,
+
   },
 ]
 
-  constructor() { }
+  constructor() { 
+    // should not write any blocking code in ctor
+  }
 
   ngOnInit(): void {
   }
--- hotelinventory/src/app/rooms/rooms.ts
+++ hotelinventory/src/app/rooms/rooms.ts
@@ -12,4 +12,5 @@ export interface RoomList {
     photos: string;
     checkinTime: Date;
     checkoutTIme: Date;
+    rating: number;
 }
