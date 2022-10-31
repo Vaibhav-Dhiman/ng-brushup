13c685fe214eac540426b5a69eb170612c297294
vaibhav dhiman <princedhiman70@gmail.com>
2022-10-31 17:45:51 +0530

    store setup

---
+++ store/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+   <hinv-header></hinv-header>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'store';
+}
