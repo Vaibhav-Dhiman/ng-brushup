c5e8085e2e43942c709238b1898e527ebb861222
vaibhav dhiman <princedhiman70@gmail.com>
2022-11-03 12:30:12 +0530

    products-frame-ui

--- store/src/app/pages/home/filters/filters.component.ts
+++ store/src/app/pages/home/filters/filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class FiltersComponent implements OnInit {
+  @Output() showCategory = new EventEmitter<string>();
   categories = ['shoes', 'sports']
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngShowCategory(category: string): void {
+    this.showCategory.emit(category);
   }
 
 }
