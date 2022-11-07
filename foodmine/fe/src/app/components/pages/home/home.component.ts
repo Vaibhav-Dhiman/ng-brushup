16c876c9db0374645f88c492443261364409874a
vaibhav dhiman <princedhiman70@gmail.com>
2022-11-07 18:03:35 +0530

    foods-search-by-tag

--- foodmine/fe/src/app/components/pages/home/home.component.ts
+++ foodmine/fe/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -11,7 +12,15 @@ export class HomeComponent implements OnInit {
 
   foods: Food[] = [];
 
-  constructor(private readonly foodService: FoodService) {this.foods = foodService.getAll();}
+  constructor(private readonly foodService: FoodService, activatedRoute: ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if(params['searchTerm'])
+        this.foods = this.foodService.getAllFoodBySearchTerm(params['searchTerm']);
+      else if(params['tag'])
+       this.foods = this.foodService.getAllFoodByTag(params['tag']);
+      else this.foods = foodService.getAll();
+    })
+  }
 
   ngOnInit(): void {
   }
