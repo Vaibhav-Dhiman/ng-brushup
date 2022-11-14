6e0b61c66df15991f01f3a1601f70d9ffeebc8b5
vaibhav dhiman <princedhiman70@gmail.com>
2022-11-14 14:30:09 +0530

    be- clean up

--- foodmine/be/src/server.ts
+++ foodmine/be/src/server.ts
@@ -1,43 +1,19 @@
 import express from "express";
 import cors from "cors";
-import { sample_foods, sample_tags, sample_users } from "./data";
+import foodRouter from './router/food.router';
+import { sample_users } from "./data";
 import jwt from "jsonwebtoken";
 
 const app = express();
 const PORT = "5000";
-
 app.use(cors({
     credentials: true,
     origin:["http://localhost:4200"]
 }));
 
 app.use(express.json());
+app.use("/api/foods", foodRouter);
 
-app.get("/api/foods", (req, res) => {
-    res.send(sample_foods);
-})
-
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-    const searchTerm = req.params.searchTerm;
-    const foods = sample_foods.filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
-    res.send(foods);
-});
-
-app.get("/api/foods/tags", (req,res) => {
-    res.send(sample_tags)
-})
-
-app.get("/api/foods/tags/:tagName", (req,res) => {
-    const tagName = req.params.tagName;
-    const tags = sample_foods.filter(food => food.tags?.includes(tagName));
-    res.send(tags);
-})
-
-app.get("/api/foods/:foodId", (req,res) => {
-    const foodId = req.params.foodId;
-    const food = sample_foods.find(food => food.id === foodId);
-    res.send(food);
-})
 
 app.post("/api/users/login", (req,res) => {
     const { email , password} = req.body;
@@ -48,7 +24,7 @@ app.post("/api/users/login", (req,res) => {
     } else {
         res.status(400).send('User name or password not valid');
     }
-})
+});
 
 const generateTokenResponse = (user: any) => {
     const token = jwt.sign({
@@ -56,7 +32,7 @@ const generateTokenResponse = (user: any) => {
     }, "somerandamtexttogeneratetokenthisiskey", {expiresIn: "1d"})
     user.token = token;
     return user;
-}
+};
 
 app.listen(PORT, ()=> {
     console.log(`App running on port ${PORT}`);
